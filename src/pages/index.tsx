13037f5c90d053ec52a72ff17ed6dcf79261c681
giant-seohyun <seohyun.sim@giantstepcorp.com>
2024-02-02 17:26:58 +0900

    feat: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        hello it's test package~
+        hello it's test package
       </main>
     </>
   );
