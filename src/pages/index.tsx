764d43d89d367e3846a4b8d1fc7708f25c9e37c9
giant-seohyun <seohyun.sim@giantstepcorp.com>
2024-02-14 18:33:08 +0900

    done: pr 자동화 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -15,7 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>hello worlds</div>
+        <div className={styles.description}>hello world</div>
       </main>
     </>
   );
