a847107dd9e815e0d5bb562d2d9a8f68884d5b0b
giant-seohyun <seohyun.sim@giantstepcorp.com>
2025-03-26 16:59:13 +0900

    커밋 테스트 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>hello worldss</div>
+        <div className={styles.description}>hello worldsssdsd</div>
       </main>
     </>
   );
